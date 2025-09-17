05f94a8aa0a00400345976facb8f80a7cdc36d99
Jingles5 <jonathan.ingles@mymail.champlain.edu>
2025-09-17 11:43:34 -0400

    saving prj

---
+++ Assets/funny.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class funny : MonoBehaviour
+{
+    bool once = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (once)
+        {
+            StartCoroutine(Funny());
+            once = false;
+        }
+    }
+
+    IEnumerator Funny()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 5f));
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        StartCoroutine(Funny());
+    }
+}
