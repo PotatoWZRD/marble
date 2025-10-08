5336ad84a32b2651afadd473a0a54a1afbde78aa
Jingles5 <jonathan.ingles@mymail.champlain.edu>
2025-10-07 23:33:08 -0400

    unity fixed

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,11 +1,15 @@
-using WebSocketSharp;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
 
 
 public class test : MonoBehaviour
 {
     WebSocket ws;
+    List<Player> ts = new List<Player>();
+    List<Player> Loader = new List<Player>();
+    [SerializeField] GameObject marbleTest;
+    public int speed;
 
     void Start()
     {
@@ -14,7 +18,8 @@ public class test : MonoBehaviour
         ws.Send("{\"type\":\"connect\",\"client\":\"unity\"}");
         ws.OnMessage += (sender, e) =>
         {
-            Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+            Debug.Log(e.Data);
+            handleMessage(e.Data);
         };
     }
 
@@ -24,18 +29,85 @@ public class test : MonoBehaviour
         {
             return;
         }
-    }/*
-    void handleMessage(string suff)
-    {
-        var data = JsonUtility.FromJson<mes>(suff);
-        if (data.typr == "")
+
+        if (Loader.Count > 0)
         {
-            
+            foreach (Player go in Loader)
+            {
+                if (go.commando == 'c')
+                {
+                    GameObject test = Instantiate(marbleTest);
+                    test.transform.position = Vector2.zero;
+                    go.player = test;
+                    if (int.TryParse(go.name.Substring(0, 3), out int id))
+                    {
+                        go.player.name = id.ToString();
+                        go.id = id;
+                    }
+                }
+                else if(go.commando == 'j')
+                {
+                    Debug.Log("Jill");  
+                    go.player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+                    string paddedID = go.id.ToString("D3");
+                    ws.Send("{\"type\":\"jumped\",\"client\":\"unity\",\"id\":\""+paddedID+"\"}");
+                }
+            }
+            Loader.Clear();
         }
     }
 
-    public class mes
+    public void handleMessage(string data)
     {
-        public string typr;
-    }*/
+        /*
+         * data will give id of obj-
+         * data will also give instructions
+         * code will look at if joined
+         * make marble on join-
+         * make marble jump if not join
+         */
+
+        string data_id = "seth";
+        char command = data[3];
+        Debug.Log(command);
+
+        switch (command)
+        {
+            case 'c':
+
+                Debug.Log("james");
+                Player player = new Player();
+                player.name = data;
+                player.commando = command;
+                Loader.Add(player);
+                ts.Add(player);
+                break;
+            case 'j':
+                Debug.Log("jame2s");
+                data_id = data.Substring(0, 3);
+                Debug.Log(data_id);
+                if (int.TryParse(data_id, out int id))
+                {
+                    Debug.Log(id.ToString());
+                    foreach (Player p in ts)
+                    {
+                        if (p.id == id)
+                        {
+                            p.commando = command;
+                            Loader.Add(p);
+                        }
+                    }
+                }
+                break;
+        }
+       
+    }
+
+    public class Player
+    {
+        public GameObject player;
+        public int id;
+        public string name;
+        public char commando;
+    }
 }
